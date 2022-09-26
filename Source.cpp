4be8a2649aa552ac00c62aee4c8679d6647c3307
Péter Lukács <peter.lukacs16@gmail.com>
2022-09-26 19:24:58 +0200

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,6 +1,8 @@
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
+#include <opencv2/opencv.hpp>
+#include <iostream>
 
 void setImageRed(cv::Mat img) {
 	img = cv::Scalar(0, 0, 100);
@@ -27,6 +29,26 @@ void FillRed() {
 
 }
 
+void decreaseBrightness()
+{
+	cv::Mat img = cv::imread("C:\\Users\\hallgato\\Desktop\\kepf09_19\\ratpepega.jpg");
+	cv::Mat image;
+	img.convertTo(image, -1, 1, -50); //decrease the brightness by 50
+	cv::imshow("brightness", image);
+}
+
+void imageBlur()
+{
+	cv::Mat image = cv::imread("C:\\Users\\hallgato\\Desktop\\kepf09_19\\ratpepega.jpg");
+	int x = 225;
+	int y = 225;
+	int w = 150;
+	int h = 150;
+	cv::Rect region(x, y, w, h);
+	cv::GaussianBlur(image(region), image(region), cv::Size(0, 0), 20);
+	cv::imshow("blur", image);
+}
+
 void ImgtoGray(cv::Mat &img) {
 	cv::Mat GrayScale;
 	cv::cvtColor(img, GrayScale, cv::COLOR_BGR2GRAY);
@@ -49,6 +71,8 @@ int main() {
 	ImgtoGray(Image);
 	FillRed();
 	convertRGBtoHSV();
+	decreaseBrightness();
+	imageBlur();
 
 	cv::imshow("M", M);
 	cv::waitKey();
